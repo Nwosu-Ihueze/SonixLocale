e53503e83dfda37e56fea803ac03fea90438ac20
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-07-14 10:09:33 -0400

    added images

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-    var carousel = $("#carousel").waterwheelCarousel({
-      flankingItems: 3,
-    });
-    $('#prev').bind('click',function () {
-      carousel.prev();
-      return false
-    });
-    $('#next').bind('click',function () {
-      carousel.next();
-      return false;
-    });
+  var carousel = $("#carousel").waterwheelCarousel({
+    flankingItems: 3,
   });
+
+  $('#prev').bind('click', function () {
+    carousel.prev();
+    return false
+  });
+
+  $('#next').bind('click', function () {
+    carousel.next();
+    return false;
+  });
+});
   
 
 
