42bfed48d99b676b8425a378e3d33b209bfae581
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-07-18 13:37:46 -0400

    mobile

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, shrink-to-fit=no">
     <title>SonixLocale</title>
     <!-- Bootstrap-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -64,6 +64,8 @@
 <link rel="stylesheet" href="./css/lightbox.css">
 <!--Intro Carousel-->
 <link href="./css/content-slider.css" rel="stylesheet">
+<!--Mobile-->
+<link rel="stylesheet" href="./css/mobile.css">
 
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700;900&display=swap" rel="stylesheet">
@@ -139,14 +141,7 @@
                       <!--/ Form-->
 
 
-                <!--Mode switcher-->
-                <div class="container">
-                        <div class="theme-switch">
-                          <a class="switch" href="dark.html"></div>
-                        </div>
-                        <!--/ Mode switcher-->
-
-                </div></div>
+                ></div>
                 <!--/Text-->
 
                 <!--Image-->
